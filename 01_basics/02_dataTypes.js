eeed4bed99509676991e886c146384e8294f7be6
Sudarshan Shah <57533068+SudarshanShah@users.noreply.github.com>
2023-09-15 20:21:28 +0000

    data types in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version
+
+let name = "Sudarshan"
+let age = 25
+let isLoggedIn = true
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "sudarshan");
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
