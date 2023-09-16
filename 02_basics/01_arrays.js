dfbfebad1ea0f47d7adf1f0837d198d5af378eb9
Sudarshan Shah <57533068+SudarshanShah@users.noreply.github.com>
2023-09-16 21:01:47 +0000

    Array basics part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["Ironman", "Superman"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+console.log(myArr[1]);
+
+
+// Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9) // insert element at start of array
+myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+// slice don't affect original array
+const myN1 = myArr.slice(1, 3)
+console.log(myN1);
+console.log("B ", myArr);
+
+// splice affects original array
+const myN2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myN2);
+
