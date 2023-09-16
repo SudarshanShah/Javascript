af2eb944db3e5fb865791fe35e4e7ffbe2528daf
Sudarshan Shah <57533068+SudarshanShah@users.noreply.github.com>
2023-09-16 20:31:07 +0000

    Date and Time in JS

---
+++ 01_basics/07_datetime.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// month start from 0
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toLocaleString());
+
+let myNewDate = new Date("01-14-2023")
+console.log(myNewDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myNewDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay() + 1);
+
+// many customizations in toLocaleString() method
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
